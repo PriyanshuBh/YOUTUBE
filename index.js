d0e3f5433dc48c23f8a467bd5cc4c7372ceefaa7
Priyanshubh <priyanshubh2003@gmail.com>
2023-11-07 14:29:06 +0530

    update

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express=require("express");
 
 const videoRouter=require("./routes/videoRoutes");
+const authRouter=require("./routes/authRoutes");
 
 const mongoose=require("mongoose");
 
 mongoose.connect("mongodb+srv://youtubeclone:1234@cluster0.5ofd4si.mongodb.net/youtubeclone_220240101107").then(()=>{
     console.log("DB connected");
-})
+});
 const app=express();
 app.use(express.json());
-
+app.use(authRouter);
 app.use(videoRouter);
 
 app.listen(3000,onServerStart);
@@ -17,3 +18,6 @@ app.listen(3000,onServerStart);
 function onServerStart(){
     console.log("server started");
 }
+
+
+
